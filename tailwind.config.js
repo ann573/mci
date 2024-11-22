18454177f15fb8ba25fa19e40c5e3eb64e34c33e
ann573 <ngthanhan0507@gmail.com>
2024-11-22 09:23:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './*.html',
+    './*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'prm_color': '#F1692F',
+      },
+    }
+  },
+  plugins: [],
+}
